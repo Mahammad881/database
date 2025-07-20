18009372fe23baabfeb60224aeecff842eaf16d5
Mahammad881 <mh5676023@gmail.com>
2025-07-20 20:56:16 +0530

    Update array.py

--- array.py
+++ array.py
@@ -2,3 +2,4 @@ print("array is main function of the python"}
 print("pytho is a high level language")
 print("python")
 print("python is data structure language")
+print("pyhton is very important ")
